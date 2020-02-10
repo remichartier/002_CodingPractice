df5dd3bd99dc8c95905dfe5baa041d6ced7e3874
remichartier <remipr.chartier@gmail.com>
2020-02-10 00:42:34 -0800

    add

--- 003_C++/20200201_1747_3in1Stacks/3In1Stacks.cc
+++ 003_C++/20200201_1747_3in1Stacks/3In1Stacks.cc
@@ -2,12 +2,14 @@
 Describe how you could use a single array to implement 3 stacks.
 
 Hints : #2, 12, 38, 58
-12 : we could simulate 3 stacks by dividing array by 3. One might actually be much bigger than the others, through. Can we be more lexible with the divisions ?
+12 : we could simulate 3 stacks by dividing array by 3. One might actually be much bigger than the others,
+through. Can we be more lexible with the divisions ?
 
-38 : if you want to allow or flexible divisions, you can shift stacks around. Can you ensure that all available capacity is used ?
+38 : if you want to allow or flexible divisions, you can shift stacks around. 
+Can you ensure that all available capacity is used ?
 
 58
-Try thinking about the array as circular, such that the end of the array "wraps around"to the start 
+Try thinking about the array as circular, such that the end of the array "wraps around" to the start 
 of the array
 
 Approach 1 : fixed division
@@ -29,6 +31,7 @@ Approach 2 : flexible divisions
 
 */
 #include<iostream>
+#include <vector>
 using namespace std;
 
 template <class T> class Node {
@@ -42,6 +45,8 @@ template <class T> class Node {
 			next = nullptr ;
 		}
 };
+
+
 
 template <class T> class Stack {
 	
@@ -74,6 +79,74 @@ template <class T> class Stack {
 		bool isEmpty(){
 			return(top == nullptr);
 		}
+};
+
+#define N 10
+#define NBSTACK 3
+
+// note : not compiled yet
+
+template <class T> class ThreeStackArray {
+	// Approach 1 : fixed division
+	/*3 main functions for stacks : 
+	isEmpty;
+	push(item);
+	item=pop();
+	peek()
+	*/
+
+	private :
+		int topIndexes[NBSTACK] ;
+		int tailIndexes[NBSTACK] ;
+		T  stack[NBSTACK*N] ;
+		
+		void testStackNb(unsigned int stackNb){
+			if(stackNb>=NBSTACK){cout<<"StackNumber does not exists"<< endl; exit();}
+		}
+		
+	public :
+		// constructor 
+		ThreeStackArray(){
+			for(unsigned int i=0;++i;i<NBSTACK){
+				topIndexes[i] = i*N ;
+				tailIndexes[i] = i*N ;
+			}
+		}
+
+		bool isEmpty(unsigned int stackNb){
+			testStackNb(stackNb);
+			if(topIndexes[stackNb] == tailIndexes[stackNb]){return true ;}
+			return false ;
+		}
+
+		bool isFull(unsigned int stackNb){
+			testStackNb(stackNb);
+			if(tailIndexes[stackNb] == ((stacknb+1)*N)) { return true ;}
+			return false ;
+ 		}
+
+		bool push(T item, unsigned int stackNb){
+			testStackNb(stackNb);
+			if(isFull(stackNb)) {cout<<"StackNumber full. Push() impossible"<< endl;return false ;}
+			stack[tailIndexes[stackNb]] = item ;
+			++tailIndexes[stackNb];
+			return true ;
+		}
+
+		bool pop(T & item, unsigned int stacknb){
+			testStackNb(stackNb);
+			if(isEmpty(stackNb)){ return false ;}
+			item = stack[tailIndexes[stackNb]-1];
+			--++tailIndexes[stackNb];
+			return true ;	
+		}
+
+		// return index in each stack. -1 if stack empty ...
+		int peek(unsigned int stacknb){
+			testStackNb(stackNb);
+			return(tailIndexes[stackNb] -1 - topIndexes[stackNb]) ;
+		}
+
 };
 
 int main(){
---
+++ 003_C++/20200209_2320_StackMin/StackMin.cc
@@ -0,0 +1,250 @@
+/* Description :
+How would you design a stack, which in addition to push and pop, has a function min() which returns the minimum element ? push, pop min should operate in O(1) time.
+Hints : #27, 59, 78
+*/
+
+/* 
+
+*/
+#include<iostream>
+using namespace std;
+
+// Not yet compiled neither used
+template <class T> class Node {
+	private : 
+		T data;
+		Node<T> * next;
+	public : 
+		// Constructor
+		Node<T> (T d){
+			data = d;
+			next = nullptr ;
+		}
+};
+
+
+// Not yet compiled neither used.
+template <class T> class Stack {
+	
+	private : 
+		Node<T> * top ;
+	public : 
+		//constructor
+		Stack(T d){
+			top = new Node<T>(d) ;
+		}
+
+		T pop(){
+			if (top == nullptr){ return NULL ;}
+			T item = top->data ;
+			top = top->next ;
+			return item ;
+		}
+
+		void push(T item){
+			Node<T> * n = new Node<T>(item) ;
+			n->next = top ;
+			top = n ;
+		}	
+
+		T peek(){ // return top item of stack
+			if( top == nullptr) {return NULL ; }
+			return top->data ;
+		}
+
+		bool isEmpty(){
+			return(top == nullptr);
+		}
+
+		void printStack(){
+			if(top == nullptr){
+				cout << "Stack is empty" << endl ;
+				return ;
+			}
+			Node<T> * p(top) ;
+			while(p!=nullptr){
+				cout << "; " << p->data << " " ;
+				p = p->next ;
+			}
+		}
+};
+
+/* Description :
+How would you design a stack, which in addition to push and pop, has a function min() which returns the minimum element ? push, pop min should operate in O(1) time.
+Hints : #27, 59, 78
+*/
+
+/* 
+- Need 2nd item in node to indicate the minimum of all values so far.
+- Because when pushing, we compare to the minimum already pushed.
+- However when popping, we do not have history of minimums so far, we would have to scroll all the stack
+to calculate the minimum, which we do not have the ability to (no methods exist to do so)
+*/
+
+template <class T> class NodeMin {
+	private : 
+		T data;
+		T stackMin;
+		NodeMin<T> * next;
+	public : 
+		// Constructor
+		NodeMin<T> (T d,T minValue){
+			data = d;
+			stackMin = minValue ;
+			next = nullptr ;
+		}
+		T getData(){
+			return data ;
+		}
+		T getMin(){
+			return stackMin ;
+		}
+		NodeMin<T> * getNext(){
+			return next ;
+		}
+		void setNext(NodeMin<T> * n){
+			next = n ;
+		}
+};
+
+template <class T> class StackMin {
+	
+	private : 
+		NodeMin<T> * top ;
+	public : 
+		//constructor
+		StackMin(T d){
+			// for the first node of the stack, minimum is the item itself.
+			top = new NodeMin<T>(d,d) ;
+		}
+
+		// destructor 
+		~StackMin(){
+			// need to pop all the nodes ....
+			while(!isEmpty()){
+				pop();
+			}
+		}
+		
+		T pop(){
+			//if (top == nullptr){ return NULL ;}
+			T item = top->getData() ;
+			NodeMin<T> * n(top) ;
+			top = top->getNext() ;
+			return item ;
+			delete n ;
+			n = nullptr ;
+		}
+
+		void push(T item){
+			// need first to collect minimum of current stack. (top->stackMin)
+			// Then compare to item and decide about new minimum.
+			T minimum = item < top->getMin() ? item : top->getMin() ;
+			NodeMin<T> * n = new NodeMin<T>(item,minimum) ;
+			n->setNext(top) ;
+			top = n ;
+		}	
+
+		T peek(){ // return top item of stack
+			if( top == nullptr) {return NULL ; }
+			return top->getData() ;
+		}
+
+		bool isEmpty(){
+			return(top == nullptr);
+		}
+
+		// add function min()
+		T min(){
+			//if( top == nullptr) {return NULL ; }
+			// return Min value of top Node ...
+			return top->getMin() ;
+		}
+
+		void printStack(){
+			if(top == nullptr){
+				cout << "Stack is empty" << endl ;
+				return ;
+			}
+			NodeMin<T> * p(top) ;
+			while(p!=nullptr){
+				cout << "; " << p->getData() << " (min : " << p->getMin() << ") " ;
+				p = p->getNext() ;
+			}
+			cout << endl ;
+		}
+
+};
+
+/* still needed to be done : 
+study how to prevent issues when top = nullptr in methods.
+study the pointers/delete needs.
+study the destructors needs.
+Study the core dump at the end.
+ 
+Test back the basic stack : compile + execute + test.
+Do same with the queue.
+
+*/
+
+int main(){
+
+	StackMin<int> * numbers = new StackMin<int>(0) ;
+	numbers->printStack() ;
+	cout << "push()" << endl ;
+	numbers->push(10);
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "push()" << endl ;
+	numbers->push(5);
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "push()" << endl ;
+	numbers->push(15);
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "push()" << endl ;
+	numbers->push(1);
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "push()" << endl ;
+	numbers->push(-1);
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	
+	cout << "pop()" << endl ;
+	numbers->pop();
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "pop()" << endl ;
+	numbers->pop();
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "pop()" << endl ;
+	numbers->pop();
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "pop()" << endl ;
+	numbers->pop();
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "pop()" << endl ;
+	numbers->pop();
+	numbers->printStack() ;
+	cout << "minStack is : " << numbers->min() << endl ;
+	cout << "pop()" << endl ;
+	//numbers->pop();
+	//numbers->printStack() ;
+	//cout << "minStack is : " << numbers->min() << endl ;
+	//numbers->pop();
+	//numbers->printStack() ;
+	//cout << "minStack is : " << numbers->min() << endl ;
+
+	delete numbers ;
+	numbers = nullptr ;
+
+	return 0;
+}
+/* g++ sortFile.cpp -o sortFile
+
+*/
