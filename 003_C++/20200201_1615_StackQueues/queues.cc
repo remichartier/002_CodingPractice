c7839beec6791d79d6489c2a1ea167a83a1bd048
remichartier <remipr.chartier@gmail.com>
2020-02-29 15:55:12 -0800

    modify queues.cc

--- 003_C++/20200201_1615_StackQueues/queues.cc
+++ 003_C++/20200201_1615_StackQueues/queues.cc
@@ -1,9 +1,4 @@
 /* Description :
-	Stack = LIFO Last In First Out
-	pop()
-	push(item)
-	peek()
-	isEmpty()	
 
 	Queue = FIFO
 	add() item at the end
@@ -22,8 +17,21 @@ template <class T> class Node {
 	public : 
 		// Constructor
 		Node<T> (T d){
+			cout << "Node constructor called" << endl ;
 			data = d;
 			next = nullptr ;
+		}
+
+		T getData(){
+			return data;
+		}
+
+		Node<T> * getNext(){
+			return next ;
+		}
+
+		void setNext(Node<T> * p){
+			next = p ;
 		}
 };
 
@@ -36,8 +44,17 @@ template <class T> class Queue {
 	public : 
 		//constructor
 		Queue(T d){
+			cout << "Queue constructor called" << endl ;
 			top = new Node<T>(d) ;
 			bottom = top ;
+		}
+
+		// destructor
+		~Queue(){
+			cout << "Queue destructor called" << endl ;
+			while(!isEmpty()){
+				remove();
+			}
 		}
 
 		void add(T item){
@@ -46,37 +63,129 @@ template <class T> class Queue {
 				top = n ;
 			}
 			else {
-				bottom->next = n ;
+				bottom->setNext(n);
 			}
 			bottom = n ;
-			n->next = nullptr ;		
+			n->setNext(nullptr) ;		
 		}
 
 		T remove(){
 			T item ;
 			if( top == nullptr){
 				cout << "remove() impossible, Queue empty." << endl ;
-				return NULL ;
+
 			}
-			item = top->data ;
-			Node<T> * toRemove(top) ;
-			top = top->next ;
-			if (top == nullptr) { bottom = nullptr ;}
-			free(toRemove);
-			return item ;
+			else {
+				item = top->getData() ;
+				Node<T> * toRemove(top) ;
+				top = top->getNext() ;
+				if (top == nullptr) { bottom = nullptr ;}
+				delete toRemove;
+				toRemove = nullptr ;
+				return item ;	
+			}
+			
 		}	
 
 		T peek(){ // return top item of stack
-			if( top == nullptr) {return NULL ; }
-			return top->data ;
+			if( top == nullptr) {
+				cout << "Queue is empty ! "; 
+			}
+			else{
+				return top->getData() ;	
+			}
+			
 		}
 
 		bool isEmpty(){
 			return (top == nullptr) ;
 		}
+
+		void printQueue(){
+		 	Node<T> * n(top);
+		 	if(isEmpty()){
+		 		cout << "Queue is empty --> printout impossible\n" ;
+		 		return ;
+		 	}
+		 	cout << "Queue printout  : "  ;
+		 	while( n != nullptr){
+		 		cout << n->getData() << " | " ;
+		 		n = n->getNext() ;
+		 	}
+		 	cout << endl ;
+		}
 };
 
 int main(){
+	// test of queues class.
+	cout << "Creating a queue" << endl ;
+	Queue<int> q = Queue<int>(5) ;
+	
+	cout << "Add 5 to queue" << endl ;
+	q.add(5) ;
+	q.printQueue();
+
+	cout << "Add 6 to queue" << endl ;
+	q.add(6) ;
+	q.printQueue();
+
+	cout << "Add 7 to queue" << endl ;
+	q.add(7) ;
+	q.printQueue();
+
+	cout << "Add 8 to queue" << endl ;
+	q.add(8) ;
+	q.printQueue();
+
+	cout << "Add 9 to queue" << endl ;
+	q.add(9) ;
+	q.printQueue();
+
+	cout << "Add 10 to queue" << endl ;
+	q.add(10) ;
+	q.printQueue();
+
+
+	cout << "TOP OF THE QUEUE : " << q.peek() << endl ;
+	
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "Process queue element" << endl ;
+	q.remove() ;
+	q.printQueue();
+
+	cout << "TOP OF THE QUEUE : " << q.peek() << endl ;
 
 	return 0;
 }
