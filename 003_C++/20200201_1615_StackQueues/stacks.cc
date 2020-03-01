ca83c808188b8b49c2cacb651a86467ddf8161c5
remichartier <remipr.chartier@gmail.com>
2020-02-29 16:38:02 -0800

    correct + test stacks.cc

--- 003_C++/20200201_1615_StackQueues/stacks.cc
+++ 003_C++/20200201_1615_StackQueues/stacks.cc
@@ -13,11 +13,23 @@ template <class T> class Node {
 	private : 
 		T data;
 		Node<T> * next;
+
 	public : 
 		// Constructor
 		Node<T> (T d){
+			cout << "Node constructor called" << endl ;
 			data = d;
 			next = nullptr ;
+		}
+
+		T getData(){
+			return data ;
+		}
+		void setNext(Node<T> * p){
+			next = p ;
+		}
+		Node<T> * getNext(){
+			return next ;
 		}
 };
 
@@ -26,35 +38,137 @@ template <class T> class Stack {
 	private : 
 		Node<T> * top ;
 	public : 
-		//constructor
+		//Constructor
 		Stack(T d){
+			cout << "Stack constructor called" << endl ;
 			top = new Node<T>(d) ;
+		}
+		//Destructor
+		~Stack(){
+			cout << "Stack destructor called" << endl ;
+			while(!isEmpty()){
+				pop();
+			}
 		}
 
 		T pop(){
-			if (top == nullptr){ return NULL ;}
-			T item = top->data ;
-			top = top->next ;
-			return item ;
+			if (isEmpty()){ 
+				cout << "Stack is empty !!! ";
+			}
+			else {
+				T item = top->getData() ;
+				Node<T> * n = top ;
+				top = top->getNext() ;
+				delete n ;
+				n = nullptr ;
+				return item ;	
+			}
 		}
 
 		void push(T item){
 			Node<T> * n = new Node<T>(item) ;
-			n->next = top ;
+			n->setNext(top) ;
 			top = n ;
 		}	
 
 		T peek(){ // return top item of stack
-			if( top == nullptr) {return NULL ; }
-			return top->data ;
+			if(isEmpty()) {
+				cout << "Stack is empty !!!" ; 
+			}
+			else{
+				return top->getData() ;	
+			}			
 		}
 
 		bool isEmpty(){
 			return(top == nullptr);
 		}
+
+		void print(){
+			if(isEmpty()){
+				cout << "Stack is empty !!!" << endl ;
+				return ;
+			}
+			cout << "Stack content : " ;
+			Node<T> * p(top) ;
+			while( p != nullptr){
+				cout << p->getData() << " | " ;
+				p = p->getNext() ;
+			}
+			cout << endl ;
+		}
 };
 
 int main(){
+
+	cout << "Stack creation with 0" << endl ;
+	Stack<int> stck = Stack<int>(0) ;
+	stck.print();
+
+	cout << "Add to stack 1" << endl ;
+	stck.push(1) ;
+	stck.print();
+
+	cout << "Add to stack 2" << endl ;
+	stck.push(2) ;
+	stck.print();
+
+	cout << "Add to stack 3" << endl ;
+	stck.push(3) ;
+	stck.print();
+
+	cout << "Add to stack 4" << endl ;
+	stck.push(4) ;
+	stck.print();
+
+	cout << "Add to stack 5" << endl ;
+	stck.push(5) ;
+	stck.print();
+
+	cout << "Add to stack 6" << endl ;
+	stck.push(6) ;
+	stck.print();
+
+	cout << "TOP OF STACK : " << stck.peek() << endl ;
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "pop() !!!!" << endl ;
+	stck.pop() ;
+	stck.print();
+
+	cout << "TOP OF STACK : " << stck.peek() << endl ;
+
 
 	return 0;
 }
