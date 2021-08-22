82a094fba6115ba8c71e17294ecf7d56c7fa319c
remichartier <remipr.chartier@gmail.com>
2021-08-21 22:36:40 -0700

    Implement Pythong Quicksort Algorithm in place

--- 001_Python/20210818_2350_UdacityQuickSortAlgo/QuickSortAlgo_v01.py
+++ 001_Python/20210818_2350_UdacityQuickSortAlgo/QuickSortAlgo_v01.py
@@ -2,7 +2,39 @@
 Input a list.
 Output a sorted list."""
 def quicksort(array):
-    return []
+    # quick sort with pivot
+    # Then quick sort left_side of pivot and quick_sort right side of pivot
+    # Then combine left_array + Pivot + Right Array
+    # but reminder : it is an "in place" Algorithm
+    #left = 0
+    #right = len(array) -1
+
+    def sort(left,right):
+        pivot = right
+        #for pos in range(left,right + 1):
+        pos = left
+        while pos != pivot and pos < right :
+            # compare value at pos and value at pivot
+            # if array(pivot) < array(pos), need to move pivot...
+            if array[pivot] < array[pos]:
+                tmp = array[pos]
+                array[pos] = array[pivot-1]
+                array[pivot-1] = array[pivot]
+                array[pivot] = tmp
+                pivot -= 1
+                pos = left
+            else:
+                pos +=1                
+        # end loop
+        # Now sort right array, since left array already sorted
+        if left < pivot -1:
+            sort(left,pivot-1)
+        if pivot+1 < right:
+            sort(pivot+1,right)
+
+
+    sort(0,len(array)-1)
+    return array
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
-print quicksort(test)
+print(quicksort(test))
