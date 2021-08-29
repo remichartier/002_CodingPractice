6c42fe9dd728d29153e5963e81a51fe5152c5113
remichartier <remipr.chartier@gmail.com>
2021-08-29 16:01:42 -0700

    Udacity Binary Tree Practice

--- 001_Python/20210828_2354_UdacityBinaryTreePractice/BinaryTreePractice_v01.py
+++ 001_Python/20210828_2354_UdacityBinaryTreePractice/BinaryTreePractice_v01.py
@@ -1,12 +1,17 @@
 """
-Now, it's your turn! Your goal is to create your own binary tree. You should start with the most basic building block:
+Now, it's your turn! Your goal is to create your own binary tree. 
+You should start with the most basic building block:
 class Node(object):
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 Every node has some value, and pointers to left and right children.
-You'll need to implement two methods: search(), which searches for the presence of a node in the tree, and print_tree(), which prints out the values of tree nodes in a pre-order traversal. You should attempt to use the helper methods provided to create recursive solutions to these functions.
+You'll need to implement two methods: search(), which searches for the presence of 
+a node in the tree, and print_tree(), 
+which prints out the values of tree nodes in a pre-order traversal. 
+You should attempt to use the helper methods provided to create recursive 
+solutions to these functions.
 Let's get started!
 Start Quiz
 Provided :
@@ -26,22 +31,48 @@ class BinaryTree(object):
         """Return True if the value
         is in the tree, return
         False otherwise."""
-        return False
+        return self.preorder_search(self.root, find_val)        
+        #return False
 
     def print_tree(self):
         """Print out all tree nodes
         as they are visited in
         a pre-order traversal."""
-        return ""
+         # pre-order traversal
+
+        if self.root == None:
+            return ""
+        return self.preorder_print( self.root, "")
 
     def preorder_search(self, start, find_val):
         """Helper method - use this to create a 
         recursive search solution."""
+
+        # pre-order traversal
+        if start == None:
+            return False
+        if start.value == find_val:
+            return True
+        if start.left:
+            if self.preorder_search(start.left, find_val):
+                return True
+        if start.right:
+            if self.preorder_search(start.right, find_val):
+                return True
         return False
 
     def preorder_print(self, start, traversal):
         """Helper method - use this to create a 
         recursive print solution."""
+
+        # pre-order traversal
+        if start == None:
+            return traversal
+        traversal += str(start.value)
+        if start.left:
+            traversal = self.preorder_print(start.left, traversal + "-")
+        if start.right:
+            traversal = self.preorder_print(start.right, traversal + "-")
         return traversal
 
 
@@ -54,10 +85,10 @@ tree.root.left.right = Node(5)
 
 # Test search
 # Should be True
-print tree.search(4)
+print(tree.search(4))
 # Should be False
-print tree.search(6)
+print(tree.search(6))
 
 # Test print_tree
 # Should be 1-2-4-5-3
-print tree.print_tree()
+print(tree.print_tree())
