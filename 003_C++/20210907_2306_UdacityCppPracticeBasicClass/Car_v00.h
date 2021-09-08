79a756306206d7a2260d953256503acbcd1b61b5
remichartier <remipr.chartier@gmail.com>
2021-09-07 23:24:24 -0700

    Udacity C++ practice classes

---
+++ 003_C++/20210907_2306_UdacityCppPracticeBasicClass/Car_v00.h
@@ -0,0 +1,18 @@
+/*
+Class Basics
+In this challenge, I want to flip the tables a bit. In the previous quizzes, I've been giving you header files and asking you to implement methods from them. In this quiz, I'm going to give you a .cpp file and ask you to write the corresponding header file.
+
+In Car.cpp, you'll find the implementation of a simple Car class. This is a very unreliable car that has a 50/50 chance of being broken after every drive.
+
+I want you to examine Car.cpp and write the corresponding header file, Car.h. The code you'll find below won't compile without a working header file. Check out the compiler errors and make it work!
+
+Note: In the below quiz, we've added comments over the top of the function definitions in the .cpp file, although typically these comments should be placed over the function declarations in the .h file instead - we've placed them here since .h is not implemented yet.
+*/
+
+// Take a look at Car.cpp to see how to define the Car class.
+
+// Hint: you'll need to define:
+// 1. the class itself
+// 2. the class constructor
+// 3. one private property
+// 4. three public methods
