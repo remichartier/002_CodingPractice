e401f52c44dc0b432e08ec6cbd06bde9819d8ae2
remichartier <remipr.chartier@gmail.com>
2021-10-08 21:43:58 -0700

    Hackerrank Matrix Diagonal Difference Easy practice in Python

---
+++ 001_Python/20211008_2136_MatrixDiagonalDifference/matrixDiagonalDifference_v01.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    n = len(arr[0])
+    right = 0
+    left = 0
+    for i in range(n):
+        right += arr[i][i]
+        left += arr[i][n -1 -i]
+    return abs(right - left)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
