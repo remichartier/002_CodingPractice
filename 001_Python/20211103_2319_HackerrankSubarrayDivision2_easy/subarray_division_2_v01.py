1aa64485df1218ef37eb9a2ec0615194bc89962f
remichartier <remipr.chartier@gmail.com>
2021-11-03 23:53:59 -0700

    Hackerrank practice Subarray Division 2 easy in Python

---
+++ 001_Python/20211103_2319_HackerrankSubarrayDivision2_easy/subarray_division_2_v01.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    # need to take m elements
+    # sums should be = to d.
+    # count the solutions.
+    count = 0
+    i = 0
+    while (i < len(s)) and ((i + m -1) < len(s)):
+        if sum(s[i:i+m]) == d:
+            count += 1
+        i += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
