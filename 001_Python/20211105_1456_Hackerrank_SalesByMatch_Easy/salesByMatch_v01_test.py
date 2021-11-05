956402c5e49e0aded3608565a2cc3906e6c58020
remichartier <remipr.chartier@gmail.com>
2021-11-05 15:46:55 -0700

    Hackerrank Sales By Match Easy Practice python

---
+++ 001_Python/20211105_1456_Hackerrank_SalesByMatch_Easy/salesByMatch_v01_test.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    count = 0
+    ar.sort()
+    i = 0
+    while i < len(ar) -2:
+        if ar[i] == ar[i+1]:
+            count +=1
+            i += 2
+        else:
+            i += 1
+    return count
+
+print(sockMerchant(7,[1,2,1,2,1,3,2]))
+print(sockMerchant(7,[10, 20, 20, 10, 10, 30, 50, 10, 20]))
+'''
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+'''
