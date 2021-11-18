586a7386d9760f23b261b128a85be6115900cb86
remichartier <remipr.chartier@gmail.com>
2021-11-17 16:18:35 -0800

    Hacckerank Left Rotation Easy in Python practice

---
+++ 001_Python/20211117_1550_HackerrankLeftRotationEasy/leftRotation_v01.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    # memorize d first elements.
+    # remove them from head of array
+    # add them to end of array
+    arr1 = arr[0:d]
+    arr2 = arr[d:]
+    arr2.extend(arr1) 
+    return arr2 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
