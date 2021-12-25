b0793c6e16159a030b519fa4f141f16799f8bfb4
remichartier <remipr.chartier@gmail.com>
2021-12-25 19:33:05 +0100

    Hackerrank Number Game Python Medium practice

---
+++ 001_Python/20211225_1127_HackerrankNumberGameMedium/numberGame_v00.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'counterGame' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def counterGame(n):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = counterGame(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
